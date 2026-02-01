57b31e902c73f7857caae2a06d6446014ea47add
Aritra <aritra.03@proton.me>
2026-02-01 21:12:51 +0530

    added origin check

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 import { Hono } from "hono";
-import { cors } from 'hono/cors'
+import { cors } from "hono/cors"
 import home from "./routes/home.js";
 import applrec from "./routes/applrec.js";
 
 const app = new Hono();
 
-app.use('*', cors({
-  origin: '*',
-  allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//handle cors
+app.use("/api/*", cors({
+  origin: "https://applrec-client.pages.dev/",
+  allowMethods: ["GET", "POST", "PUT", "DELETE"],
 }))
+
+//only let the frontend make request
+app.use("/api/*", async function (c, next) {
+  const origin = c.req.header("Origin");
+  if (origin !== "https://applrec-client.pages.dev/") {
+    return c.json({
+      message: "You are not allowed !!"
+    }, 403);
+  }
+
+  await next();
+});
 
 app.route("/", home);
 app.route("/api", applrec);
