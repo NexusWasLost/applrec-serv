f0aa9a7b302ffaded69b89af0233f55b8bdb5302
Aritra <aritra.03@proton.me>
2026-02-10 19:35:06 +0530

    Added update and search-by-id endpoint

--- routes/applrec.js
+++ routes/applrec.js
@@ -63,8 +63,6 @@ applrec.get("/search-by-date", async function (c) {
             }, 400);
         }
 
-        console.log(date);
-
         const sql = neon(c.env.REMOTE_DB_URL);
         let data = await sql.query(`
             SELECT * FROM applrec
@@ -89,6 +87,71 @@ applrec.get("/search-by-date", async function (c) {
     }
 });
 
+applrec.get("/search-by-id", async function(c){
+    try{
+        const id = c.req.query("id");
+        if (!id) {
+            return c.json({
+                message: "No id provided"
+            }, 400);
+        }
+
+        const sql = neon(c.env.REMOTE_DB_URL);
+        let data = await sql.query(`
+            SELECT
+            TO_CHAR(appldate, 'YYYY-MM-DD') AS appldate,
+            companyname, position, url, status, notes
+            FROM applrec WHERE appl_id = ${ id }
+        `);
+
+        if (data.length === 0) {
+            data = "No Matching fields";
+        }
+
+        return c.json({
+            message: "Fetch for Update Successful",
+            data: data
+        }, 200);
+    }
+    catch(error){
+        console.log(error);
+        return c.json({
+            message: "Failed to Search"
+        }, 500);
+    }
+})
+
+applrec.put("/update-appl", async function(c){
+    try{
+        const { id, appldate, companyname, position, url, status, notes } = await c.req.json();
+        if(!id){
+            return c.json({
+                message: "No id provided"
+            }, 400);
+        }
+
+        const sql = neon(c.env.REMOTE_DB_URL);
+        const data = await sql.query(`
+        UPDATE applrec
+        SET
+        appldate = '${ appldate }', companyname = '${ companyname }',
+        position = '${ position }', url = '${ url }',
+        status = '${ status }', notes = '${ notes }'
+        WHERE appl_id = ${ id }
+        `);
+
+        return c.json({
+            message: "Update Data recieved successfully"
+        }, 200);
+    }
+    catch(error){
+        console.log(error);
+        return c.json({
+            message: "Failed to Search"
+        }, 500);
+    }
+})
+
 applrec.post("/apply", async function (c) {
     try {
         const { appldate, companyname, position, url, status, notes } = await c.req.json();
@@ -96,7 +159,7 @@ applrec.post("/apply", async function (c) {
 
         const data = await sql.query(`
         INSERT INTO applrec (appldate, companyname, position, url, status, notes)
-        VALUES('${ appldate }', '${ companyname }', '${ position }', '${ url }', '${ status }', '${ notes }')
+        VALUES('${appldate}', '${companyname}', '${position}', '${url}', '${status}', '${ notes }')
         `);
 
         return c.json({
